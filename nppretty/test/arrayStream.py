838ae9faee495b22cd40268bb57295bbdc2a8cf5
telamonian <mklein@jhu.edu>
2018-11-11 21:52:03 -0500

    added test

--- nppretty/test/arrayStream.py
+++ nppretty/test/arrayStream.py
@@ -3,7 +3,7 @@ from nppretty import *
 if __name__=='__main__':
     import numpy as np
 
-    with ArrayStream('arraystream.txt', 'w', name='arr1D') as f:
+    with ArrayStream('arraystream.txt', name='arr1D') as f:
         for i in range(10):
             arr = np.arange(10*i, 10*(i + 1))
             f.write(arr)
@@ -12,3 +12,9 @@ if __name__=='__main__':
         for i in range(10):
             arr = np.arange(10*i, 10*(i + 1))
             f.write(arr.reshape(2,5))
+
+    arrstr = ArrayStream('arraystream.txt', name='arr2D_noContext')
+    for i in range(10):
+        arr = np.arange(10*i, 10*(i + 1))
+        arrstr.write(arr.reshape(2,5))
+    arrstr.close()
